06951971cb694fa615ee218351ffc9b28b45de51
Agradeep Khanra <agradeep92@gmail.com>
2016-06-06 23:36:07 -0500

    Added tests for countdown form

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,12 +1,28 @@
 var React = require('react');
 var Clock = require('Clock');
+var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
+
+	getInitialState: function() {
+		return { intSecs: 0 };
+	},
+
+	onSetCountdown: function(data) {
+
+		this.setState({
+			intSecs: data
+		});
+	},
+
 	render: function() {
+
+		var {intSecs} = this.state;
 		return (
 			<div>
 				<h1>CountdownJSX</h1>
-				<Clock totalSecs={129} />
+				<Clock totalSecs={intSecs} />
+				<CountdownForm onSetCountdown = {this.onSetCountdown} />
 			</div>
 		);
 	}
---
+++ app/components/CountdownForm.jsx
@@ -0,0 +1,28 @@
+var React = require('react');
+
+var CountdownForm = React.createClass({
+	
+	onSubmit: function(event) {
+		event.preventDefault();
+		var strSecs = this.refs.inputSecs.value;
+
+		if(strSecs.match(/^[0-9]*$/) && strSecs.length > 0) {
+			this.props.onSetCountdown(parseInt(strSecs, 10));
+		}
+
+		this.refs.inputSecs.value = '';
+	},
+
+	render: function() {
+		return (
+			<div>
+				<form ref="form" className="countdown-form" onSubmit={this.onSubmit}>
+					<input type="text" ref="inputSecs" placeholder="Please enter a time in seconds..." />
+					<button className="button expanded hollow">Start Countdown</button>
+				</form>
+			</div>
+		);
+	}
+});
+
+module.exports = CountdownForm;
